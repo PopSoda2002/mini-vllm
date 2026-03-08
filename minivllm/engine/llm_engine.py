d0e5c0a6eaaadecf6260edc351dcbdfa5a51f58a
PopSoda2002 <zhouhp.me@gmail.com>
2026-03-08 09:23:02 +0000

    [minivllm] Implement basic engine and loader

---
+++ minivllm/engine/llm_engine.py
@@ -0,0 +1,25 @@
+from dataclasses import fields
+import torch.multiprocessing as mp
+from transformers import AutoTokenizer
+
+from minivllm.config import Config
+from minivllm.engine.model_runner import ModelRunner
+
+class LLMEngine:
+    def __init__(self, model, **kwargs):
+        config_fields = {field.name for field in fields(Config)}
+        config_kwargs = {k: v for k, v in kwargs.items() if k in config_fields}
+        config = Config(model, **config_kwargs)
+        self.ps = []
+        self.events = []
+        ctx = mp.get_context("spawn")
+        for i in range(1, config.tensor_parallel_size):
+            event = ctx.Event()
+            process = ctx.Process(target=ModelRunner, args=(config, i, event))
+            process.start()
+            self.ps.append(process)
+            self.events.append(event)
+        self.model_runner = ModelRunner(config, 0, self.events)
+        self.tokenizer = AutoTokenizer.from_pretrained(config.model, use_fast=True)
+        config.eos = self.tokenizer.eos_token_id
+        self.scheduler = Scheduler(config)
