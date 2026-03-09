403a7dbebddf3d62d8f0ce3f3a4ec009e7d9f2ce
PopSoda2002 <zhouhp.me@gmail.com>
2026-03-09 18:07:04 +0000

    [minivllm] Add Qwen3 model

--- minivllm/models/qwen3.py
+++ minivllm/models/qwen3.py
@@ -1,13 +1,14 @@
 import torch
 from torch import nn
 import torch.distributed as dist
+from transformers import Qwen3Config
 
 from minivllm.layers.activation import SiluAndMul
 from minivllm.layers.attention import Attention
 from minivllm.layers.layernorm import RMSNorm
 from minivllm.layers.linear import QKVParallelLinear, MergedColumnParallelLinear, RowParallelLinear
 from minivllm.layers.rotary_embedding import get_rope
-
+from minivllm.layers.embed_head import VocabParallelEmbedding
 class Qwen3Attention(nn.Module):
     def __init__(
         self,
@@ -108,3 +109,66 @@ class Qwen3MLP(nn.Module):
         x = self.act_fn(gate_up)
         x = self.down_proj(x)
         return x
+
+class Qwen3DecoderLayer(nn.Module):
+    def __init__(
+        self,
+        config: Qwen3Config,
+    ) -> None:
+        super().__init__()
+        rope_scaling = None
+        self.self_attn = Qwen3Attention(
+            hidden_size=config.hidden_size,
+            num_heads=config.num_attention_heads,
+            num_kv_heads=config.num_key_value_heads,
+            max_position=config.max_position_embeddings,
+            rms_norm_eps=config.rms_norm_eps,
+            qkv_bias=getattr(config, 'attention_bias', True),
+            head_dim=getattr(config, 'head_dim', None),
+            rope_theta=getattr(config, "rope_theta", 1000000),
+            rope_scaling=rope_scaling,
+        )
+        self.mlp = Qwen3MLP(
+            hidden_size=config.hidden_size,
+            intermediate_size=config.intermediate_size,
+            hidden_act=config.hidden_act,
+        )
+        self.input_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self,
+        positions: torch.Tensor,
+        hidden_states: torch.Tensor,
+        residual: torch.Tensor | None,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        if residual is None:
+            hidden_states, residual = self.input_layernorm(hidden_states), hidden_states
+        else:
+            hidden_states, residual = self.input_layernorm(hidden_states, residual)
+        hidden_states = self.self_attn(positions, hidden_states)
+        hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
+        hidden_states = self.mlp(hidden_states)
+        return hidden_states, residual
+
+class Qwen3Model(nn.Module):
+    def __init__(
+        self,
+        config: Qwen3Config,
+    ) -> None:
+        super().__init__()
+        self.embed_tokens = VocabParallelEmbedding(config.vocab_size, config.hidden_size)
+        self.layers = nn.ModuleList([Qwen3DecoderLayer(config) for _ in range(config.num_hidden_layers)])
+        self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        positions: torch.Tensor,
+    ) -> torch.Tensor:
+        hidden_states = self.embed_tokens(input_ids)
+        residual = None
+        for layer in self.layers:
+            hidden_states, residual = layer(positions, hidden_states, residual)
+        hidden_states, _ = self.norm(hidden_states, residual)
+        return hidden_states
